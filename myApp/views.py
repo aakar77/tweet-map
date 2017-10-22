10861b48a292ec26994901f99c07ab817f3081fd
Aalap Parikh <aalap@172-17-82-237.DYNAPOOL.NYU.EDU>
2017-10-22 00:32:58 -0400

    Edited views.py

--- myApp/views.py
+++ myApp/views.py
@@ -1,7 +1,36 @@
 from django.shortcuts import render
 import json
 import time
+#Import the necessary methods from tweepy library
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+from elasticsearch import Elasticsearch
+from elasticsearch import Elasticsearch, RequestsHttpConnection
+from requests_aws4auth import AWS4Auth
+#import logstash
+#import logging
+#from .event import Event
+import json
+import sys
 
+
+AWS_ACCESS_KEY = 'AKIAJMALUJ4KNADPAUDA'
+AWS_SECRET_KEY = 'dEnyd7DOx+b1Xw+f0rCHxlpr44daWmZxZ2Xt+avu'
+region = 'us-east-2' # For example, us-east-1
+service = 'es'
+
+awsauth = AWS4Auth(AWS_ACCESS_KEY, AWS_SECRET_KEY, region, service)
+
+host = 'search-tweetmap101-tytzonkx3fobbdw6egsdaem5zy.us-east-2.es.amazonaws.com' # For example, my-test-domain.us-east-1.es.amazonaws.com
+
+es = Elasticsearch(
+    hosts = [{'host': host, 'port': 445}],
+    http_auth = awsauth,
+    use_ssl = True,
+    verify_certs = True,
+    connection_class = RequestsHttpConnection
+)
 # Create your views here.
 
 # Topic List to be populated in the Dropdown menu
@@ -29,5 +58,18 @@ def tweetsearch(request):
                     { "lat": 28.018349 , "long": -82.764473, "tweetData": "DOnald" },
                     { "lat": 34.426388, "long": -117.300880, "tweetData": "Virginia" }
                 ]}
-
+    gettweets(searchText)
     return render(request, 'myApp/mymap.html',{'tweet_dict':data_dict,'topics':topic, 'search':searchText})
+
+
+
+def gettweets(search_term):
+    res = es.search(q='the')
+    tweets = list()
+    #print res
+    for x in res['hits']['hits']:
+        tweets.append(x['_source'])
+    #print tweets
+    return tweets
+    
+    
