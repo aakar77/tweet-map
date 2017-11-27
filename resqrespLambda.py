f7be17d2c009cca91271c132fedb348ccabf4140
aakar77 <aakarjinwala@gmail.com>
2017-11-27 01:09:04 -0500

    changed the API gateway resquest handler, parsed the request body

--- resqrespLambda.py
+++ resqrespLambda.py
@@ -6,13 +6,24 @@ import datetime
 import time
 import sys
 
-def lambda_handler(event, context):
+def stringParsing(myString):
+
+    whitelist = set('abcdefghijklmnopqrstuvwxy  :ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+    key = ''.join(filter(whitelist.__contains__, myString))
+    return(key[9:])
+
+def request_handler(event, context):
 
     print("Received event: " + json.dumps(event, indent=2))
 
-    current_time = datetime.datetime.now().time()
+    key = "not present"
 
-    key1 = "not present"
+    if event['body'] is not None:
+        #data = event['body']
+        key = stringParsing(event['body'])
+        print(key)
+
+    # For data[6:] "atus' : 'Trump'\n}" is response
 
     ## Code for fectching the user keyword from the request
     # Extract from event['body']
@@ -30,14 +41,16 @@ def lambda_handler(event, context):
     response = {
         "statusCode": 200,
         "headers": {
-        "Access-Control-Allow-Origin" : 'https://s3.us-east-2.amazonaws.com'
+            "Access-Control-Allow-Origin" : 'https://s3.us-east-2.amazonaws.com'
         },
-        "body": json.dumps(body)
+        "body": json.dumps(key)
     }
 
     return response
 
+'''
 if __name__ == '__main__':
-  event = {'body' : { 'status' : 'Modi'} }
-  mytest = lambda_handler(event,"b")
+  event = {'body' : "{ 'status' : 'Modi'}" }
+  #event = "{/\n status : Trump }"
+  mytest = request_handler(event,"b")
 '''
